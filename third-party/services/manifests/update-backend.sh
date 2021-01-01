37bd1b1a621e42247b24c8a4291f29434d0d328a
Riccardo Carlesso <ricc+derek.zrh@google.com>
2021-01-01 14:21:17 +0100

    copied

---
+++ third-party/services/manifests/update-backend.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+gsutil cp backend.yml gs://spinnaker-gcs-bucket-lrsq05m9x1hllf653020-1609165495/manifests/backend.yml
---
+++ third-party/services/manifests/update-frontend.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+gsutil cp frontend.yml gs://spinnaker-gcs-bucket-lrsq05m9x1hllf653020-1609165495/manifests/frontend.yml
